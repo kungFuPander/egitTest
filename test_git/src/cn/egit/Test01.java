26ad6ae01437c26bd5dde6ae4afd9f210826260e
kungFuPander <13782079607@163.com>
2019-11-20 22:51:18 +0800

    create project for test git

---
+++ test_git/src/cn/egit/Test01.java
@@ -0,0 +1,16 @@
+package cn.egit;
+
+public class Test01 {
+	
+	public static void main(String[] args) {
+		System.out.println("拉取");
+		System.out.println("删除");
+		System.out.println("新增");
+		System.out.println("提交");
+		System.out.println("修改");
+		System.out.println("删除");
+		System.out.println("冲突");
+		System.out.println("分支");
+	}
+
+}
